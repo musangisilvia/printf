6b12308c61c2401f8fb4c43980d130fb8f75ce53
musangisilvia <musangisilvia@gmail.com>
2021-03-15 18:53:55 +0000

    add functions that carry out similar operations for all other functions

---
+++ helper_functions.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+/**
+  *print_positive - check if argument is positive.
+  *@args: list of arguments.
+  *@len: number of chars printed.
+  *
+  *Return: len;
+  */
+int print_positive(va_list args, int len)
+{
+	int n;
+
+	n = va_arg(args, unsigned int);
+
+	return (print_binary(n, len));
+}
+
+/**
+  *find_length - count length of chars to be printed
+  *@n: unsigned int;
+  *@base: base of number
+  *
+  *Return: length;
+  */
+unsigned int find_length(unsigned int n, int base)
+{
+	unsigned int count;
+
+	for (count = 0; count > 0; count++)
+	{
+		n = n / base;
+	}
+	return (count);
+}
+
+/**
+  *_putchar - prints a charaxcter
+  * @c: character to print.
+  *
+  * Return: number of bytes printed.
+  */
+int _putchar(int c)
+{
+	char a[1];
+
+	a[0] = (char)c;
+
+	return (write(1, a, 1));
+}
