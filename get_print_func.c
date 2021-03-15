337d251490079f7e947839b7d5056979f7da9822
Alfred Tuva <muindetuva@gmail.com>
2021-03-15 08:37:07 +0000

    Add get print function

---
+++ get_print_func.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+  * get_print_func - Get the correct print function and return a pointer to it
+  * @c: The specifier or current character
+  * @b: The previous character
+  * @args: List of arguments
+  *
+  * Return: Pointer to correct print function
+  */
+
+int get_print_func(char c, char b, va_list args, int len)
+{
+	switch (c)
+	{
+		case 'c':
+			len = print_char(args, len);
+			break;
+		case 's':
+			len = print_str(args, len);
+			break;
+		case '%':
+			_putchar(c);
+			len += _putchar(c);
+			break;
+		case 'd':
+		case 'i':
+			len = print_num(args, len);
+			break;
+		case 'b':
+			len = print_unsigned(args, len);
+			break;
+		default:
+			len += _putchar(b);
+			len += _putchar(c);
+			break;
+	}
+	return (len);
+}
