bbbbc57b344d27bf6be455e5229e62dcc84540de
Alfred Tuva <muindetuva@gmail.com>
2021-03-15 08:37:27 +0000

    Add header file

---
+++ holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(int c);
+int _printf(const char *format, ...);
+
+int get_print_func(char, char, va_list, int); 
+int print_char(va_list, int);
+int print_str(va_list, int);
+int print_num(va_list, int);
+int putchar_int(int, int);
+int print_unsigned(va_list, int);
+int print_binary(int, int);
+#endif
