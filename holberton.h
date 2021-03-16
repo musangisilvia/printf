3862bf596fa52c8e4b77401bcd303e2c72d5e457
Alfred Tuva <muindetuva@gmail.com>
2021-03-16 07:08:27 +0000

    Add prototypes for the new functions

--- holberton.h
+++ holberton.h
@@ -16,5 +16,8 @@ int putchar_int(int, int);
 int print_positive(va_list, int);
 int print_binary(int, int);
 unsigned int find_length(unsigned int n, int base);
+int print_unsigned(va_list args, int len); 
+int print_numbers(unsigned long n, unsigned int base, const char *digits);
+
 
 #endif /* HOLBERTON_H */
